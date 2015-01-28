ca87ae133af393b4240e7746fd49df7e86f4040c
Michael McFarland <mikedmcfarland@gmail.com>
2015-01-28 14:44:58 -0500

    converted wrap to manipulate

--- index.js
+++ index.js
@@ -1,80 +1,94 @@
 //Metal-smith dependencies
 var Metalsmith  = require('metalsmith')
-    markdown    = require('metalsmith-markdown')
-    templates   = require('metalsmith-templates')
-    collections = require('metalsmith-collections')
-    permalinks  = require('metalsmith-permalinks')
-    drafts      = require('metalsmith-drafts')
-    excerpts    = require('metalsmith-excerpts')
-    less        = require('metalsmith-less')
-    feed        = require('metalsmith-feed')
-    watch       = require('metalsmith-watch')
-    fileMetaData= require('metalsmith-filemetadata')
-    headings    = require('metalsmith-headings')
+var markdown    = require('metalsmith-markdown')
+var templates   = require('metalsmith-templates')
+var collections = require('metalsmith-collections')
+var permalinks  = require('metalsmith-permalinks')
+var drafts      = require('metalsmith-drafts')
+var excerpts    = require('metalsmith-excerpts')
+var less        = require('metalsmith-less')
+var feed        = require('metalsmith-feed')
+var watch       = require('metalsmith-watch')
+var fileMetaData= require('metalsmith-filemetadata')
+var headings    = require('metalsmith-headings')
 
 var updatePaths = require('./metalsmith/updatePaths')
-    wrap        = require('./metalsmith/wrap')
-    minimist    = require('minimist')
+var wrap        = require('./metalsmith/wrap')
+
+var minimist    = require('minimist')
+var shelljs     = require('shelljs')
+
 
 var argv = minimist(process.argv.slice(2))
 var devMode = argv._.indexOf('dev') !== -1
 
+var postsPattern = 'posts/**/*.{html,md}'
+var pagesPattern = 'pages/**/*.{html,md}'
 
 var collectionsDefs = {
-    posts: {
-        pattern: 'posts/*.md',
-        reverse: true
-    },
-    pages: {
-        pattern: 'pages/*.md'
-    }
+  posts: {
+    pattern: postsPattern,
+    reverse: true
+  },
+  pages: {
+    pattern: pagesPattern
+  }
 }
 
 var root = __dirname;
+var buildDir = root + '/build'
+
+//clean it ourselves, since metalsmith blasts away our
+//hidden files
+shelljs.rm('-rf',buildDir + '/*')
+
 var metalSmith = Metalsmith(root)
-    .metadata({
-        site : {
-             title : 'Thrown for a loop',
-             url: 'http://thrownforaloop.com',
-             author: 'Michael McFarland'
-        }
-    })
-    .destination(root + '/build')
-    .use(drafts())
-    .use(fileMetaData([
-        {pattern: 'posts/*.md', preserve: true, metadata: {template:'post.jade'}},
-        {pattern: 'pages/*.md', preserve: true, metadata: {template:'page.jade'}}
-    ]))
-    .use(collections(collectionsDefs))
-    .use(markdown())
-    .use(excerpts())
-    .use(headings(['h1','h2']))
-    .use(wrap())
-       // .use(less())
-    .use(permalinks({
-        pattern: ':collection/:title'
-    }))
-    .use(updatePaths)
-    .use(templates('jade'))
-    .use( feed({
-        collection : 'posts'
-    }))
-    // .use(function(files,metalsmith,done){
-    //     console.log('files',files,'metalsmith',metalsmith)
-    //     console.log('metalsmith._metadata.posts[0]',metalsmith._metadata.posts[0])
-    //     console.log('metalsmith._metadata.posts[1]',metalsmith._metadata.posts[1])
-    //    done()
-    // })
+  .clean(false)
+  .metadata({
+    site : {
+      title : 'Thrown for a loop',
+      url: 'http://thrownforaloop.com',
+      author: 'Michael McFarland'
+    }
+  })
+  .destination(buildDir)
+  .use(drafts())
+  .use(fileMetaData([
+    {pattern: postsPattern, preserve: true, metadata: {template:'post.jade'}},
+    {pattern: pagesPattern, preserve: true, metadata: {template:'page.jade'}}
+  ]))
+  .use(collections(collectionsDefs))
+  .use(markdown())
+  .use(excerpts())
+  .use(headings(['h1','h2']))
+  .use(wrap())
+  // .use(wrap({
+  //   selector: 'pre.src'
+  // }))
+// .use(less())
+  .use(permalinks({
+    pattern: ':collection/:title'
+  }))
+  .use(updatePaths)
+  .use(templates('jade'))
+  .use( feed({
+    collection : 'posts'
+  }))
+// .use(function(files,metalsmith,done){
+//     console.log('files',files,'metalsmith',metalsmith)
+//     console.log('metalsmith._metadata.posts[0]',metalsmith._metadata.posts[0])
+//     console.log('metalsmith._metadata.posts[1]',metalsmith._metadata.posts[1])
+//    done()
+// })
 
 if(devMode){
-    metalSmith.use(watch({
-        livereload: true
-    }))
+  metalSmith.use(watch({
+    livereload: true
+  }))
 }
 
 metalSmith
-    .build(function(err, files) {
-        if(err)
-            console.error('error building',err)
-    })
-
+  .build(function(err, files) {
+    if(err)
+      console.error('error building',err)
+  })
--- metalsmith/wrap.js
+++ metalsmith/wrap.js
@@ -1,44 +1,85 @@
-var cheerio = require('cheerio')
-	extname = require('path').extname
-	_	    = require('lodash')
-
+var extname = require('path').extname
+var _	    = require('lodash')
+var jquery  = require('jquery')
+var jsdom   = require('jsdom')
+var Promise = require('promise')
 
 function wrap (options) {
-	var settings = _.extend({},{
-		selector : 'pre,.mxgraph',
-		wrapper : '<div class="invert"></div>',
-	},options)
+  var settings = _.extend({},{
+    selector : 'pre,.mxgraph',
+    wrapper : '<div class="invert"></div>'
+  },options)
 
-	var selector = settings.selector
-	var wrapper  = settings.wrapper
+  var selector = settings.selector
+  var wrapper  = settings.wrapper
 
-	return function(files,metalSmith,done) {
+  return function(files,metalSmith,done) {
 
-		Object.keys(files).forEach(function(file) {
-			if(!isHtmlFile(file))
-				return
+    var alterations = _(files)
+      .keys()
+      .filter(isHtmlFile)
+      .map(function(file) {
+        var data = files[file]
+        return makeDom(data.contents.toString())
+          .then(doWrap).then(function(html) {
+            console.log('got result')
+            data.contents = new Buffer(html)
+          })
+      })
+      .value()
 
-			var data = files[file]
-		 	var $ = cheerio.load(data.contents.toString())	
-		 	var elements = $(selector)
+    Promise.all(alterations).done(function(r) {
+      console.log('d0ne')
+      done()
+    },console.error)
+  }
 
-		 	var pairs = _.map(elements,function(e){
-		 		return {element: $(e), wrap:$(wrapper) }
-		 	})
+  function doWrap($){
+    $(selector).wrap(wrapper)
+    var html = $('body').html()
 
-		 	pairs.forEach(function(p) {
-		 		
-		 		p.element.replaceWith(p.wrap)
-		 		p.wrap.append(p.element)
-		 	})
+    return html
 
-		 	var str = $.html()
-		 	data.contents = new Buffer(str)
-		})
+    // // console.log("elements = ", elements);
+    // var pairs = _.map(elements,function(e){
+    //   return {element: e, wrap:$(wrapper) }
+    // })
+    // console.log("pairs = ", pairs);
 
-		done()	
-	}
+    // pairs.forEach(function(p) {
+    //   p.element.replaceWith(p.wrap)
+    //   p.wrap.append(p.element)
+    // })
+
+    // var str = $.html()
+    // return str
+  }
 }
+
+// function makeDom(html){
+//   return new Promise(function(fulfill){
+//     var window = jsdom.jsdom(html).parentWindow
+//     jsdom.jQueryify(window,"http://code.jquery.com/jquery.js",function() {
+//       fulfill(window.$)
+//     })
+//   })
+// }
+
+function makeDom(html){
+  return new Promise(function(fulfill){
+    jsdom.env(
+      html,
+      ['http://code.jquery.com/jquery.js'],
+      function(errors,window) {
+        if(errors){
+          reject(errors)
+        }
+        fulfill(window.$)
+      }
+    )
+  })
+}
+
 
 function isHtmlFile(file){
   return /\.html?/.test(extname(file));
