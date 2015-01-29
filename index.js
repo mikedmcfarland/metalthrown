163669dcfeeb2a3d92f00e39415a6a20bfecd96b
Michael McFarland <mikedmcfarland@gmail.com>
2015-01-29 13:11:59 -0500

    more tweaks to accomadate org

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ var feed        = require('metalsmith-feed')
 var watch       = require('metalsmith-watch')
 var fileMetaData= require('metalsmith-filemetadata')
 var headings    = require('metalsmith-headings')
+var ignore      = require('metalsmith-ignore')
 
 var updatePaths = require('./metalsmith/updatePaths')
-var wrap        = require('./metalsmith/wrap')
+var manipulate  = require('./metalsmith/manipulate')
 
 var minimist    = require('minimist')
 var shelljs     = require('shelljs')
+var _           = require('lodash')
 
 
 var argv = minimist(process.argv.slice(2))
@@ -38,9 +40,9 @@ var collectionsDefs = {
 var root = __dirname;
 var buildDir = root + '/build'
 
-//clean it ourselves, since metalsmith blasts away our
-//hidden files
-shelljs.rm('-rf',buildDir + '/*')
+// //clean it ourselves, since metalsmith blasts away our
+// //hidden files
+// console.log('cleaning build')
 
 var metalSmith = Metalsmith(root)
   .clean(false)
@@ -52,6 +54,10 @@ var metalSmith = Metalsmith(root)
     }
   })
   .destination(buildDir)
+  // .use(ignore([
+  //   "*.js",
+  //   "*.css"
+  // ]))
   .use(drafts())
   .use(fileMetaData([
     {pattern: postsPattern, preserve: true, metadata: {template:'post.jade'}},
@@ -61,17 +67,40 @@ var metalSmith = Metalsmith(root)
   .use(markdown())
   .use(excerpts())
   .use(headings(['h1','h2']))
-  .use(wrap())
-  // .use(wrap({
-  //   selector: 'pre.src'
-  // }))
-// .use(less())
+  .use(manipulate(function($) {
+    //wrap some stuff to invert it visually
+    $('pre,mxgraph').wrap('<div class="invert"></div>')
+
+    //fix org modes markup so its nicely highlighted by prism
+    $('pre.src').wrapInner('<code></code>')
+    var mapping = [
+      {org:"src-emacs-lisp", prism:"scheme" }
+    ]
+    $('pre.src').each(function(){
+      var el = $(this)
+
+      //find org to prism css class mapping
+      var codeType = _.find(mapping,function(e) {
+        return el.hasClass(e.org)
+      })
+
+      //if one exists, remove the added org classes
+      //and add the prism one
+      if(codeType !== undefined){
+        el.removeClass()
+        $('code',el).addClass('language-'+codeType.prism)
+      }
+    })
+
+  }))
+  .use(manipulate(function($){
+  }))
   .use(permalinks({
     pattern: ':collection/:title'
   }))
   .use(updatePaths)
   .use(templates('jade'))
-  .use( feed({
+  .use(feed({
     collection : 'posts'
   }))
 // .use(function(files,metalsmith,done){
@@ -81,14 +110,15 @@ var metalSmith = Metalsmith(root)
 //    done()
 // })
 
-if(devMode){
-  metalSmith.use(watch({
-    livereload: true
-  }))
-}
-
+// if(devMode){
+//   metalSmith.use(watch({
+//     livereload: true
+//   }))
+// }
+console.log('starting build')
 metalSmith
   .build(function(err, files) {
     if(err)
       console.error('error building',err)
+    console.log('finished build')
   })
